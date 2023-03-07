348f4af4ea40d71bca0229c09d791d8e913f2b04
Katsute <58778985+Katsute@users.noreply.github.com>
2023-03-07 17:40:47 -0500

    initialize dependencies

--- project/build.js
+++ project/build.js
@@ -1,1 +1,25 @@
 const fs = require("fs");
+const path = require("path");
+const sass = require("sass");
+
+//
+
+let folder = path.join(__dirname, "site", "dependencies", "css");
+
+fs.existsSync(folder) || fs.mkdirSync(folder, {recursive: true});
+fs.writeFileSync(path.join(folder, "style.css"), sass.compile(path.join(__dirname, "src", "dependencies", "css", "style.scss")).css);
+
+//
+
+folder = path.join(__dirname, "site", "dependencies", "js");
+fs.existsSync(folder) || fs.mkdirSync(folder, {recursive: true});
+
+fs.copyFileSync(
+    path.join(__dirname, "node_modules", "bootstrap", "dist", "css", "bootstrap.min.css"),
+    path.join(__dirname, "site", "dependencies", "css", "bootstrap.min.css")
+);
+
+fs.copyFileSync(
+    path.join(__dirname, "node_modules", "bootstrap", "dist", "js", "bootstrap.min.js"),
+    path.join(__dirname, "site", "dependencies", "js", "bootstrap.min.js")
+);
