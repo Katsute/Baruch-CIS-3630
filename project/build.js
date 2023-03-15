76d3cc913956e1110908aaf56f94e1470e1e0d0a
Katsute <58778985+Katsute@users.noreply.github.com>
2023-03-15 18:03:38 +0000

    add js replacers

--- project/build.js
+++ project/build.js
@@ -23,3 +23,29 @@ fs.copyFileSync(
     path.join(__dirname, "node_modules", "bootstrap", "dist", "js", "bootstrap.min.js"),
     path.join(__dirname, "site", "dependencies", "js", "bootstrap.min.js")
 );
+
+//
+
+const def = fs.readFileSync(path.join(__dirname, "template", "default.html"), "utf-8");
+
+for(const file of fs.readdirSync(path.join(__dirname, "src"), {withFileTypes: true})
+    .filter(f => f.isFile())
+    .map(f => f.name)
+    .filter(f => f.endsWith(".html")))
+    fs.writeFileSync(
+        path.join(__dirname, "site", file),
+        def // {{ content }}
+            .replace(/( *){{ *content *}}/gm, (_, indent,) =>
+                fs // {{ .html }}
+                    .readFileSync(path.join(__dirname, "src", file), "utf-8")
+                    .replace(/( *){{(.*)}}/gm, (_, indent, template) =>
+                        fs.readFileSync(path.join(__dirname, "template", template.trim()), "utf-8")
+                            .replace(/^/gm, indent)
+                    )
+                    .replace(/^/gm, indent)
+            )
+            .replace(/(<!--.*?-->)|(<!--[\S\s]+?-->)|(<!--[\S\s]*?$)/g, '') // comments
+            .replace(/^ +$/gm, "") // trim lines
+            .replace(/(?:\r?\n){2,}/gm, '\n'), // extra newlines
+        "utf-8"
+    );
